f9a5bc591181c1837386e0903063146a93f5394e
Franz <franz.cortez@alumnos.ucn.cl>
2020-11-16 00:27:02 -0300

    Menu administrador
    despliegue de todos los usuarios registrados en la base de datos, dando las opciones de añadir, eliminar o editarlos

---
+++ resources/views/auth/adminView/menuAdmin.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div align = 'center'>
+            <div class="col-5">
+                <div class = "panel-heading">
+                    <div class="card">
+                        <br>
+                        <br>
+                        <h1>MENU ADMINISTRADOR</h1>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>        
+    </div>
+    
+    <div class="panel-body">
+        <div class="card-body">
+        <table class="table">
+            <thead class="thead">
+                <tr class = "bg-primary text-white" >
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Rol</th>
+                <th scope="col">email</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Eliminar</th>
+                <a type="button" class="btn btn-outline-primary" style = "margin-bottom:20px;float:right" href="register">Agregar Usuario</a>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{$user->id}}</th>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->rol_usuario}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>
+                            <form method = "post" action ="{{ route ('menuAdmin.update',$user->id) }}">
+                                @method('update')
+                                @csrf
+                                <button class = "btn btn-primary btn-sm"> Editar Usuario</button>
+                            </form>
+                        </td>
+                        <td>
+                            <form method = "post" action ="{{ route ('menuAdmin.destroy',$user->id) }}">
+                                @method('delete')
+                                @csrf
+                                <button class = "btn btn-danger btn-sm"> Eliminar Usuario</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        </div>
+    </div>
+    
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,18 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
+                    
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    
                     You are logged in!
+
+                    @if(auth::user()->rol_usuario=='administrador')
+                        <a href="menuAdmin" class="btn btn-primary btn-lg active" role="button" aria-pressed="true"> Siguiente</a>                    
+                    @endif
                 </div>
             </div>
         </div>
