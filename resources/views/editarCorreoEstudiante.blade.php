f09cd07fc3b9c37796c9d3f5ace16bbb8547ec0e
Franz <franz.cortez@alumnos.ucn.cl>
2020-12-07 23:23:04 -0300

    Advance: ADM-004 is clear, validation is missing
    "jefe de carrera" can change the student's email but validation is missing

--- resources/views/editarCorreoEstudiante.blade.php
+++ resources/views/editarCorreoEstudiante.blade.php
@@ -1,13 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-    @if($estudiantes == null)
-        <h1>chao</h1>
-    @else
-    <h1>{{$estudiantes->rut_estudiante}}</h1>
-    <h1>{{$estudiantes->nombre_estudiante}}</h1>
-    <h1>{{$estudiantes->correo_estudiante}}</h1>
-    <h1>{{$estudiantes->apellidoPaterno_estudiante}}</h1>
-    <h1>hola</h1>
-    @endif
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Editar Usuario</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" action="{{route('viewMenuPrincipal.update', $estudiantes->id)}}" method="POST">
+                        @method('PACH')
+                        @csrf
+                        {{ csrf_field() }}
+                        {{method_field('PUT')}}
+
+                        <div>
+                            <label for="correo_estudiante" class="col-md-4 control-label">Nombre del Estudiante:</label>
+                            <label for="correo_estudiante" class="col-md-4 control-label">{{$estudiantes->nombre_estudiante}}</label>
+                            <label for="correo_estudiante" class="col-md-4 control-label">Apellido Paterno del Estudiante:</label>
+                            <label for="correo_estudiante" class="col-md-4 control-label">{{$estudiantes->apellido_paterno}}</label>
+                            <label for="correo_estudiante" class="col-md-4 control-label">Apellido Materno del Estudiante:</label>
+                            <label for="correo_estudiante" class="col-md-4 control-label">{{$estudiantes->apellido_materno}}</label>
+                            <label for="correo_estudiante" class="col-md-4 control-label">RUT del Estudiante:</label>
+                            <label for="correo_estudiante" class="col-md-4 control-label">{{$estudiantes->rut_estudiante}}</label>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('correo_estudiante') ? ' has-error' : '' }}">
+                            <label for="correo_estudiante" class="col-md-4 control-label">Correo del Estudiante</label>
+
+                            <div class="col-md-6">
+                                <input id="$estudiantes->correo_estudiante" type="text" class="form-control" name="correo_estudiante" value="{{ $estudiantes->correo_estudiante}}">
+
+                                
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Actualizar
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
