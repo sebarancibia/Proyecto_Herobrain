795af5a79b4c9fefed4548811f5d7c8d2d816efd
sebastian <sebastian.arancibia@alumnos.ucn.cl>
2020-12-19 12:53:34 -0300

    Advance: REP-001

---
+++ app/Http/Controllers/FichaController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Estudiante;
+use App\situacion;
+use App\Atencion;
+
+class FichaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $atenciones=Atencion::all();
+        $situaciones=situacion::all();
+        $estudiantes=Estudiante::all();
+        $estudianteMostrar=null;
+        return view('generarFicha',compact('estudiantes', 'estudianteMostrar','atenciones','situaciones'));
+    }
+
+    /**
+     * Metodo que buscar el estudiante y lo retorna
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function buscarEstudianteRut(Request $request)
+    {
+        $atenciones=Atencion::all();
+        $situaciones=situacion::all();
+        $estudiantes=Estudiante::all();
+        $rut_estudiante = $request->rut_estudiante;
+        $estudianteMostrar=Estudiante::where('rut_estudiante',$rut_estudiante)->first();
+        if(Estudiante::where('rut_estudiante',$rut_estudiante)->first() == null) {
+            return back()->with('error', 'El RUT o Nombre del estudiante no existe');
+        }
+        return view('generarFicha',compact('estudianteMostrar','estudiantes','atenciones','situaciones'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
